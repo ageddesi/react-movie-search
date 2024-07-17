7210634318895897d193c89a51391eec259479e7
Aaron Rackley (EverydayTinkerer) <aaron.rackley9@gmail.com>
2024-07-17 21:22:37 +0100

    feature: user can filtered the page of results

--- movie-search/src/app/page.tsx
+++ movie-search/src/app/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
   const [appLoading, setAppLoading] = useState(false);
   const [appError, setAppError] = useState(null as null | string);
   const [movies, setMovies] = useState([] as Movie[])
+  const [filteredMovies, setFilteredMovies] = useState([] as Movie[])
   const [peformedSearch, setPerformedSearch] = useState(false)
   const [currentSearchTerm, setCurrentSearchTerm] = useState("")
   const [paginationData, setPaginationData] = useState({
@@ -19,6 +20,8 @@ export default function Home() {
     totalPages: 0,
     perPage: 9
   })
+  const [genres, setGenres] = useState([] as string[])
+  const [filterBy, setFilterBy] = useState('none')
 
   const handleSearchSubmit = async (params: { page?: number, limit?: number, search: string, genre?: string }) => {
     setAppLoading(true)
@@ -28,7 +31,6 @@ export default function Home() {
       limit: paginationData.perPage, 
       page: paginationData.currentPage
     });
-    debugger;
     if(movies.success){
       setMovies(movies.body.movieData)
       setPaginationData({...paginationData, ...{ totalPages: movies.body.totalPages, page : params.page}})
@@ -45,13 +47,50 @@ export default function Home() {
   }
 
   useEffect(() => {    
-    
-  }, [])
+    if(movies.length > 0){
+      const uniqueGenres = new Set();
+      for (const movie of movies) {
+          for (const genre of movie.genres) {
+              uniqueGenres.add(genre.title);
+          }
+      }
+      const uniqueGenresArray: string[] = Array.from(uniqueGenres, (genre : any) => genre.toString()); 
+      setGenres(uniqueGenresArray)
+
+      if(filterBy !== 'none'){
+        const filteredMovies = movies.filter(movie => 
+          movie.genres.some(genre => genre.title === filterBy)
+        );
+        setFilteredMovies(filteredMovies)
+      } else {
+        setFilteredMovies(movies)
+      }
+
+    } else {
+      setGenres([])
+    }
+  }, [movies, filterBy])
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <p className="text-xl text-gray-500 mt-5">Search for a movie</p>
-      <SearchBar handleSubmit={(value) => {handleSearchSubmit({search: value})}} />
+      <SearchBar handleSubmit={(value : string) => {handleSearchSubmit({search: value})}} />
+      {genres.length > 0 && (
+        <div className="flex gap-2 mt-2"> 
+          <div>Filter results by genre:
+          </div>
+          <select name="cars" id="cars" onChange={(event) => {
+              setFilterBy(event.target.value)
+            }}>
+            <option key={'none'} value="none">No Filter</option>
+            {genres.map((genre) => {
+              return (
+                <option key={genre} value={genre}>{genre}</option>
+              )
+            })}
+          </select>
+        </div>
+      )}
       {appLoading && (
         <div className="mt-5 bg-white flex px-4 py-2">
           <p>Loading...</p>
@@ -59,11 +98,7 @@ export default function Home() {
       )}
       {peformedSearch && movies.length > 0 && (
         <>
-          <Pagination 
-            total={paginationData.totalPages + paginationData.perPage }
-            perPage={paginationData.perPage} 
-            currentPage={paginationData.currentPage} callBack={handlePaginationChange} />
-          <MovieCoverList movies={movies} />
+          <MovieCoverList movies={filteredMovies} />
           <Pagination 
             total={paginationData.totalPages + paginationData.perPage }
             perPage={paginationData.perPage} 
